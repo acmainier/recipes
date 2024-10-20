a5bc1850280db85673ce57be46e5b98778a16c93
Anne-Cecile Mainier <annececile.mainier@gmail.com>
2024-10-20 18:16:50 +0100

    Adds title, ingredient and steps props to Recipe

--- app.jsx
+++ app.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Recipe from "./Recipe";
+import data from "./recipes.json";
 
 export default function App() {
-    return <Recipe />;
+  const { title, ingredients, steps } = data[0];
+  console.log(title, ingredients, steps)
+  return <Recipe title={title} ingredients={ingredients} steps={steps} />;
 }
