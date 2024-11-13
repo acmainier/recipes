04ea032dfb741043e760fa9e6416dac91d8af55d
Anne-Cecile Mainier <annececile.mainier@gmail.com>
2024-11-13 15:04:02 +0000

    Remove button and navigation components + adding buttons in App.jsx

--- app.jsx
+++ app.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Recipe from "./Recipe";
 import data from "./recipes.json";
 
 export default function App() {
-  const recipe = data[0];
+  const [recipeId, setRecipeId] = useState(0);
+  const recipe = data[recipeId];
   const { title, ingredients, steps } = recipe;
-  console.log(title, ingredients, steps)
-  return <Recipe title={title} ingredients={ingredients} steps={steps} />;
+  return (
+    <div>
+      <div>
+        {data.map((item) => (
+          <button
+            key={item.index}
+            onClick={() => {
+              setRecipeId(item.index);
+            }}
+          >
+            {item.title}
+          </button>
+        ))}
+      </div>
+      <Recipe title={title} ingredients={ingredients} steps={steps} />
+    </div>
+  );
 }
