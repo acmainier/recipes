90b8dfb8db5d20478b8177b7e3ff1c06b6ca1da5
Anne-Cecile Mainier <annececile.mainier@gmail.com>
2025-01-22 10:47:22 +0000

    creating default page

---
+++ src/default.jsx
@@ -0,0 +1,10 @@
+
+export default function DefaultPage() {
+  
+    return (
+      <div id="default-page">
+        <h1>Welcome to my recipes App</h1>
+        <p>Use the above menu to start browsing. Let&apos;s get cooking.</p>
+      </div>
+    );
+  }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Root, { loader as rootLoader, action as rootAction } from "./Root";
 import ErrorPage from "./error-page";
-
+import DefaultPage from "./default";
 import RecipePage, { loader as recipeLoader } from "./RecipePage";
 import NewRecipe from "./newRecipe";
 import EditRecipe, { loader as editRecipeLoader } from "./editRecipe";
-import DeleteRecipe, {loader as deleteRecipeLoader} from "./deleteRecipe";
+import DeleteRecipe, { loader as deleteRecipeLoader } from "./deleteRecipe";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -17,6 +17,10 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     action: rootAction,
     children: [
+      {
+        path: "/recipes/defaultPage",
+        element: <DefaultPage />,
+      },
       {
         path: "/recipes/:recipeId",
         loader: recipeLoader,
